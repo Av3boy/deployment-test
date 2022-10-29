3bf399a5031b67bcad2bb2c6c042921247b37eea
Antti Veikkolainen <antti.veikkolainen00@outlook.com>
2022-10-29 18:16:07 +0300

    Add init projects

---
+++ deployment-test/deployment-test/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace deployment_test
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public static string Print()
+        {
+            return "fail";
+        }
+    }
+}
---
+++ deployment-test/deployment-testTests/Class1.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Tests2
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test()
+        {
+            string asd = deployment_test.Program.Print();
+            Assert.AreEqual(asd, "asd");
+        }
+    }
+}
